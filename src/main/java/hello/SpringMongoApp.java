7b020b39e6e3332bdd3b65e1dccfa26b27dfc6e4
HaiboZhu <hbzhu@thoughtworks.com>
2017-02-14 15:08:44 +0800

    [hbzhu] feat: change jar package name.

--- src/main/java/hello/SpringMongoApp.java
+++ src/main/java/hello/SpringMongoApp.java
@@ -3,11 +3,14 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Profile("application-dev.properties")
 public class SpringMongoApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringMongoApp.class, args);
     }
+
 }
